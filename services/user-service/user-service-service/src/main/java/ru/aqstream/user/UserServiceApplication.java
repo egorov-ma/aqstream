ce6ed4ba4ff6d5c7b5392756e609d11e10b7858b
egorovma <link@egorovma.ru>
2025-12-24 15:36:41 +0400

    fix(user-service): resolve JPA repository bean conflict with JpaConfig

--- services/user-service/user-service-service/src/main/java/ru/aqstream/user/UserServiceApplication.java
+++ services/user-service/user-service-service/src/main/java/ru/aqstream/user/UserServiceApplication.java
@@ -2,13 +2,12 @@ package ru.aqstream.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * Точка входа для User Service.
+ * JPA конфигурация в JpaConfig.
  */
 @SpringBootApplication
 @EnableScheduling
@@ -16,14 +15,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
     "ru.aqstream.user",
     "ru.aqstream.common.security",
     "ru.aqstream.common.web",
-    "ru.aqstream.common.messaging"
-})
-@EntityScan(basePackages = {
-    "ru.aqstream.user",
-    "ru.aqstream.common.messaging"
-})
-@EnableJpaRepositories(basePackages = {
-    "ru.aqstream.user",
     "ru.aqstream.common.messaging"
 })
 public class UserServiceApplication {
