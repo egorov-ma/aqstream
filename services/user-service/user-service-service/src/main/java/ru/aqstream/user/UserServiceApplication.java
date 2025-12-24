695959d039f6c7ce092f9f4ad4df389ceacb3d6e
egorovma <link@egorovma.ru>
2025-12-24 14:33:19 +0400

    fix: secure Redis, add Liquibase changelogs, fix EventPublisher scanning

--- services/user-service/user-service-service/src/main/java/ru/aqstream/user/UserServiceApplication.java
+++ services/user-service/user-service-service/src/main/java/ru/aqstream/user/UserServiceApplication.java
@@ -2,7 +2,9 @@ package ru.aqstream.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
@@ -13,7 +15,16 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @ComponentScan(basePackages = {
     "ru.aqstream.user",
     "ru.aqstream.common.security",
-    "ru.aqstream.common.web"
+    "ru.aqstream.common.web",
+    "ru.aqstream.common.messaging"
+})
+@EntityScan(basePackages = {
+    "ru.aqstream.user",
+    "ru.aqstream.common.messaging"
+})
+@EnableJpaRepositories(basePackages = {
+    "ru.aqstream.user",
+    "ru.aqstream.common.messaging"
 })
 public class UserServiceApplication {
 
