a551c5786f17eba5d5a5e01a8144177737365458
egorovma <link@egorovma.ru>
2025-12-25 11:34:00 +0400

    fix(event-service): add common-messaging to component scan and JPA config

--- services/event-service/event-service-service/src/main/java/ru/aqstream/event/EventServiceApplication.java
+++ services/event-service/event-service-service/src/main/java/ru/aqstream/event/EventServiceApplication.java
@@ -3,15 +3,19 @@ package ru.aqstream.event;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * Точка входа для Event Service.
+ * JPA конфигурация в JpaConfig.
  */
 @SpringBootApplication
+@EnableScheduling
 @ComponentScan(basePackages = {
     "ru.aqstream.event",
     "ru.aqstream.common.security",
-    "ru.aqstream.common.web"
+    "ru.aqstream.common.web",
+    "ru.aqstream.common.messaging"
 })
 public class EventServiceApplication {
 
--- services/event-service/event-service-service/src/main/java/ru/aqstream/event/config/JpaConfig.java
+++ services/event-service/event-service-service/src/main/java/ru/aqstream/event/config/JpaConfig.java
@@ -1,14 +1,23 @@
 package ru.aqstream.event.config;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * Конфигурация JPA для Event Service.
+ * Включает репозитории и entities из common-messaging для Outbox pattern.
  */
 @Configuration
 @EnableJpaAuditing
-@EnableJpaRepositories(basePackages = "ru.aqstream.event.db.repository")
+@EnableJpaRepositories(basePackages = {
+    "ru.aqstream.event.db.repository",
+    "ru.aqstream.common.messaging"
+})
+@EntityScan(basePackages = {
+    "ru.aqstream.event.db.entity",
+    "ru.aqstream.common.messaging"
+})
 public class JpaConfig {
 }
